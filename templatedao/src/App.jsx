647887d9e373af0d557c75d4a7877f9810ddd0f3
maryhu <mary@mintexchange.io>
2022-01-14 17:07:35 -0500

    init

---
+++ templatedao/src/App.jsx
@@ -0,0 +1,19 @@
+import {useEffect, useMemo, useState} from 'react';
+import {useWeb3} from '@3rdweb/hooks'
+import {ThirdwebSDK, VoteModule} from '@3rdweb/sdk';
+import { ethers } from 'ethers';
+
+
+const App = () => {
+   
+  
+  
+    return (
+      <div className="mint-nft">
+        <h1>😻😻 Loading</h1>
+       
+      </div>
+    );
+  };
+
+export default App;
