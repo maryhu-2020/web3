1c8802dda8183a10c380f6cbe93c885bb195de3e
maryhu <mary@mintexchange.io>
2022-02-21 22:31:10 -0500

    amm

---
+++ starknetapp/src/components/amm/InitPool.tsx
@@ -0,0 +1,51 @@
+import React, {useState, FormEvent,ChangeEvent} from "react";
+import { useStarknet,useStarknetInvoke } from "@starknet-react/core";
+import { useAMMContract } from "~/hooks/amm";
+import {useForm} from "~/hooks/form"
+
+export function InitPool(){
+    const {account} = useStarknet()
+    const {contract:ammContract} = useAMMContract()
+    const {invoke} = useStarknetInvoke({ contract:ammContract, method:'init_pool'})
+    
+    const initialState =
+    {  
+        NKI:0,
+        NDL:0
+    }
+
+    async function initPool() {
+        
+    }
+
+    const { onChange,onSubmit,values} = useForm(initPool, initialState)
+
+    if (!account){
+        return null
+    }
+
+    return(        
+        <form onSubmit={onSubmit}>
+            <h1>Initialize Pool</h1>
+            <label>Number of NKI:
+                <input 
+                    type="number" 
+                    name="NKI" 
+                    id="NKI"
+                    placeholder="0"
+                    onChange={onChange}
+                />
+            </label>
+            <label>Number of NDL:
+                <input 
+                    type="number" 
+                    name="NDL" 
+                    id="NDL"
+                    placeholder="0"
+                    onChange={onChange}
+                />
+                </label>
+                <input type="submit" />
+            </form>
+    )
+}
--- starknetapp/src/pages/amm/index.tsx
+++ starknetapp/src/pages/amm/index.tsx
@@ -1,11 +1,62 @@
 import type {NextPage} from 'next'
 
+import {ConnectWallet} from '~/components/ConnectWallet'
+import { TransactionList } from '~/components/TransactionList'
+import {InitPool} from '~/components/amm/InitPool'
+
+import {useAMMContract} from '~/hooks/amm'
+import { useStarknetCall } from '@starknet-react/core'
 
 
 const AMMHome: NextPage = () =>{
+    const {contract: ammContract} = useAMMContract()    
+    const account_id = ammContract?.connectedTo
+
+    const {data:token_a_balance} = useStarknetCall(
+        {
+            contract:ammContract, 
+            method:'get_pool_token_balance',
+            args:{token_type:'1'}
+        })
+    const {data:token_b_balance} = useStarknetCall(
+        {
+            contract:ammContract, 
+            method:'get_pool_token_balance',
+            args:{token_type:'2'}
+        })
+    const {data:acct_token_a_balance} = useStarknetCall(
+        {
+            contract:ammContract,
+            method:'get_account_token_balance', 
+            args:{ 'account_id':'0x04336d6bd82293962d8607e9574dc4410fd564db05d80c9bf4716892067a167e', 'token_type':'1'}
+        } )
+    const {data:acct_token_b_balance} = useStarknetCall(
+        {
+            contract:ammContract,
+            method:'get_account_token_balance', 
+            args:{ 'account_id':'0x04336d6bd82293962d8607e9574dc4410fd564db05d80c9bf4716892067a167e', 'token_type':'2'}
+        } )    
+
     return(
-        <div>
-            <h2>AMM Home ....</h2>
+        <div>            
+            <h2>Wallet</h2>
+            <ConnectWallet />
+            <h2>AMM Contract</h2>
+            <p>Address: {ammContract?.connectedTo}</p>
+
+            <h2>Pool Token Balance</h2>
+            <p>Token A: {token_a_balance?.balance}</p>    
+            <p>Token B: {token_b_balance?.balance}</p>   
+
+            <h2>Account Token Balance</h2>
+            <p>Token A: {acct_token_a_balance?.balance}</p>     
+            <p>Token B: {acct_token_b_balance?.balance}</p>   
+
+
+            <InitPool />
+
+            <h2>Recent Transactions</h2>
+            <TransactionList />        
         </div>
     );
 }
